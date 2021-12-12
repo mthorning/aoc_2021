20ba3bdfae833f90d40f2fd8338e555db2aa3e6e
Matthew Thorning <matthew@ometria.com>
2021-12-12 12:11:11 +0000

    day two, part 2

---
+++ src/lib/two/second.rs
@@ -0,0 +1,77 @@
+enum Direction {
+    Up,
+    Down,
+    Forward,
+}
+
+struct Instruction {
+    direction: Direction,
+    units: usize,
+}
+
+impl Instruction {
+    fn from(input: &Vec<String>) -> Vec<Instruction> {
+        input
+            .iter()
+            .map(|horizontal| {
+                let parts: Vec<&str> = horizontal.split(" ").collect();
+                Instruction {
+                    direction: Self::get_direction(parts[0]),
+                    units: parts[1].parse::<usize>().unwrap(),
+                }
+            })
+            .collect()
+    }
+
+    fn get_direction(direction: &str) -> Direction {
+        match direction {
+            "up" => Direction::Up,
+            "down" => Direction::Down,
+            "forward" => Direction::Forward,
+            _ => panic!("Input includes unknown instruction"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Submarine {
+    aim: usize,
+    horizontal: usize,
+    depth: usize,
+}
+
+impl Submarine {
+    fn new() -> Submarine {
+        Submarine {
+            aim: 0,
+            horizontal: 0,
+            depth: 0,
+        }
+    }
+
+    fn plot_route(&mut self, instructions: &Vec<Instruction>) {
+        for Instruction { direction, units } in instructions {
+            match direction {
+                Direction::Up => self.aim -= units,
+                Direction::Down => self.aim += units,
+                Direction::Forward => {
+                    self.horizontal += units;
+                    self.depth += self.aim * units;
+                }
+            }
+        }
+    }
+
+    fn multiply(&self) -> usize {
+        self.horizontal * self.depth
+    }
+}
+
+pub fn second() {
+    let input = crate::utils::get_input("two");
+    let instructions = Instruction::from(&input);
+
+    let mut submarine = Submarine::new();
+    submarine.plot_route(&instructions);
+    println!("{:?}", submarine.multiply());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
 fn main() {
-    /* aoc::one::first();
-    aoc::one::second(); */
-
-    aoc::two::first();
+    aoc::two::second();
 }
